cdc56d7203f42b526259ca5f9636a0c216442531
Chirayu31 <chirayupancholi17@gmail.com>
2024-03-09 20:21:20 +0530

    interactive follow btn

--- src/lib/components/Profile/BioAndLink.svelte
+++ src/lib/components/Profile/BioAndLink.svelte
@@ -8,6 +8,13 @@
   export let companyLogo: string
   export let position: string
   export let website: string
+
+  let follow = false
+
+  function handleFollow(e: Event) {
+    e.preventDefault()
+    follow = !follow
+  }
 </script>
 
 <div class="flex flex-col gap-4">
@@ -26,8 +33,9 @@
   <div class="flex justify-between items-center">
     <Button
       class="px-8 bg-white hover:bg-blue-600 hover:text-white text-black drop-shadow-[0_4px_0px_rgba(100,100,100,1)] hover:drop-shadow-[0_4px_0px_rgba(30,64,175,1)] "
+      on:click={handleFollow}
     >
-      Follow
+      {follow ? 'Followed' : 'Follow'}
     </Button>
     <div class="flex items-center gap-2">
       <a href={website}>
